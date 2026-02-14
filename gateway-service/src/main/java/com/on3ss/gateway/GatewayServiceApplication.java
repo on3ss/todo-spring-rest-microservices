977e6a62eeb633cf3568eee212fda07b2b0bc593
Onessimus Lyngdoh <auditoree9@gmail.com>
2026-02-14 23:44:31 +0530

    moved route bean to config file

--- gateway-service/src/main/java/com/on3ss/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/on3ss/gateway/GatewayServiceApplication.java
@@ -3,9 +3,6 @@ package com.on3ss.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,15 +11,4 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
-	@Bean
-	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("auth-service", r -> r.path("/api/auth/**")
-						.uri("lb://AUTH-SERVICE"))
-				.route("todo-service", r -> r.path("/api/todos/**")
-						.uri("lb://TODO-SERVICE"))
-				.build();
-	}
-
 }
