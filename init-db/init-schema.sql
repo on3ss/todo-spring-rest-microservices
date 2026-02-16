d3ad78e7390e62b957292c8bf8102be705fc0bf4
Oness <auditoree9@gmail.com>
2026-02-16 07:03:29 +0000

    separate pg instance for auth and todo services

--- init-db/init-schema.sql
+++
@@ -1,7 +0,0 @@
--- Create schemas for microservices isolation
-CREATE SCHEMA IF NOT EXISTS auth_schema;
-CREATE SCHEMA IF NOT EXISTS todo_schema;
-
--- Optional: Set search path or permissions if needed
--- GRANT ALL PRIVILEGES ON SCHEMA auth_schema TO ${POSTGRES_USERNAME};
--- GRANT ALL PRIVILEGES ON SCHEMA todo_schema TO ${POSTGRES_USERNAME};
